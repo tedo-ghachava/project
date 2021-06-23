d1a0eea46bbda9c1c05678d1ad88398e333696b5
tedo-ghachava <ghachava5@gmail.com>
2021-06-24 00:45:51 +0400

    added burger functional

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-document.getElementById('toggle') 
+let navbar = document.getElementById('navBar')
+let toggleButton = document.getElementById('burger-menu')
+
+toggleButton.addEventListener('click', function(){
+    this.classList.toggle('active')
+    navbar.classList.toggle('active')
+})
